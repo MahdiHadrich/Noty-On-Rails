50c2c36dd1e10fe5771b3b88e5a63218e4ae4be9
mahdihadrich <mahdo.ha@gmail.com>
2019-12-06 21:53:19 +0100

    Setting notes API

---
+++ noteApi/app/controllers/notes_controller.rb
@@ -0,0 +1,13 @@
+class NotesController < ApplicationController
+    def index
+        notes = Note.all
+        render json: notes, status: :ok
+    end
+    def create
+        note = Note.create!(note_params)
+        render json: note, status: :ok
+    end
+    def note_params
+        params.require(:note).permit(:text)
+    end
+end
